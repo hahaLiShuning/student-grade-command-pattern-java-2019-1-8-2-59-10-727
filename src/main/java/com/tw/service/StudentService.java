d366a0f00f12749493b403d9af8f4555b7b47563
LIShuning <lishiuning1955142623@outlook.com>
2019-06-14 11:42:20 +0800

    student info service completed

--- src/main/java/com/tw/service/StudentService.java
+++ src/main/java/com/tw/service/StudentService.java
@@ -1,4 +1,43 @@
 package com.tw.service;
 
+import com.tw.model.Student;
+import com.tw.utils.FileUtils;
+import com.tw.utils.TextFormatter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StudentService {
+    private TextFormatter formatter;
+
+    public StudentService() {
+        this.formatter = new TextFormatter();
+    }
+
+    public boolean addStudent(Student student) {
+        if (!isExist(student)) {
+            FileUtils.writeRecord(student.toString());
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isExist(Student student) {
+        List<String> records = FileUtils.readRecord();
+        return records.stream()
+                .map(formatter::formatStudent)
+                .map(x -> x.getNumber())
+                .filter(x -> x == student.getNumber())
+                .count() > 0;
+    }
+
+    public List<Student> findByNums(List<String> num) {
+        System.out.println(num.get(0));
+        List<String> records = FileUtils.readRecord();
+        return records.stream()
+                .map(formatter::formatStudent)
+                .filter(x -> num.contains(x.getNumber()))
+                .collect(Collectors.toList());
+    }
 }
